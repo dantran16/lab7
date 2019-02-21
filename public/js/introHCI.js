ad1a70b983f08efda31031db3a2472a9d57a3b19
dantranucsd <33334144+dantranucsd@users.noreply.github.com>
2019-02-20 22:50:28 -0800

    slide 38

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
  * Function that is called when the document is ready.
  */
 function initializePage() {
-	$(".likeBtn").click(function(){
+	$(".likeCtr").click(function(){
 		gtag("send", "event", "like", "click");
 	});
 }
